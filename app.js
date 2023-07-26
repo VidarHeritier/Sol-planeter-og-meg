c69709f79f9179f20472a3b671ec5a851775e770
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2023-07-26 10:23:47 +0200

    Trying to hide virtual kb

--- app.js
+++ app.js
@@ -99,3 +99,12 @@ initialWeight.oninput = function () {
 rocket.addEventListener("click", () => {
   rocket.remove();
 });
+
+const hideMobileKeyboardOnReturn = (element) => {
+  element.addEventListener("keyup", (keyboardEvent) => {
+    const key = keyboardEvent.code || keyboardEvent.keyCode;
+    if (key === "Enter" || key === 13) {
+      element.blur();
+    }
+  });
+};
